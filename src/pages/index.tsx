15a45317ddba6fee26e69b20bdaf17564be62325
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-10-24 15:09:14 +1100

    feat: complete sign up page to communicate with aws amplify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { useUser } from '../context/AuthContext';
 
 const Home: NextPage = () => {
+  const { user } = useUser();
+  console.log('user: ', user);
+
   return <Typography variant="h1"> hello world</Typography>;
 };
 
